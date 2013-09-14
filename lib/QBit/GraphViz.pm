85c4f5ca2ca1462da4f27e3afce876903de1c39a
Sergei Svistunov <sergei-svistunov@yandex.ru>
2013-09-14 14:34:08 +0400

    Initial commit

---
+++ lib/QBit/GraphViz.pm
@@ -0,0 +1,25 @@
+package QBit::GraphViz;
+
+use qbit;
+
+use base qw(GraphViz);
+
+sub _as_generic {
+    my ($self, $type, $dot, $output) = @_;
+
+    utf8::encode($dot);
+
+    return $self->SUPER::_as_generic($type, $dot, $output);
+}
+
+sub as_svg {
+    my ($self, $type, $dot, $output) = @_;
+
+    my $data = $self->SUPER::as_svg($type, $dot, $output);
+
+    utf8::decode($data);
+
+    return $data;
+}
+
+TRUE;
